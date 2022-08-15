2c4c5852696d059c5fcd95f976faeff39bb40517
z <18475140601@163.com>
2022-08-15 22:43:13 +0800

    feat: 配置解析请求体和cookie

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import { port } from '../config/default.config';
 import dbConnect from './utils/db_connect';
 import logger from './utils/logger';
@@ -6,11 +7,19 @@ import ioConnection from './socket.io';
 
 const app = express();
 
+// 解析 application/json 请求格式
 app.use(express.json());
+// 解析 application/x-www-form-urlencoded 请求格式
+app.use(express.urlencoded({ extended: true }));
+
+// cookie 相关插件
+app.use(cookieParser());
 
 // 启动
 const server = app.listen(port, async () => {
-  logger.info(`App is running at http://localhost:${port}`);
+  logger.info(
+    `仿微信聊天室后台服务已启动, 主页请访问: http://localhost:${port}`
+  );
   await dbConnect();
 });
 
