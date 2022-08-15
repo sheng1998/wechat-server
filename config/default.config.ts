5a3a2a9d28a27d33fd6ed2f8e01b1a5cd358797d
z <18475140601@163.com>
2022-08-15 22:49:59 +0800

    fix: 补充提交

--- config/default.config.ts
+++ config/default.config.ts
@@ -1,8 +1,12 @@
 const port = 3002;
 
+const routerConfig = {
+  prefix: '/api/v1',
+};
+
 const mongoose = {
   url: 'mongodb://127.0.0.1:27017/wechat',
   options: {},
 };
 
-export { port, mongoose };
+export { port, routerConfig, mongoose };
